660df11533f9aec68e02383e0c24694b2033cdbe
hoshikwyw <khaingwutyiwin1712@gmail.com>
2024-10-28 11:19:42 +0630

    Category Buttons done

---
+++ data/categories.ts
@@ -0,0 +1,32 @@
+const destinationCategoreis = [
+    {
+        title: "All",
+        iconName: "hiking",
+    },
+    {
+        title: "Beaches",
+        iconName: "beach",
+    },
+    {
+        title: "Mountains",
+        iconName: "terrain",
+    },
+    {
+        title: "Cities",
+        iconName: "city",
+    },
+    {
+        title: "Forests",
+        iconName: "tree",
+    },
+    {
+        title: "Islands",
+        iconName: "island",
+    },
+    {
+        title: "Deserts",
+        iconName: "weather-sunny",
+    }
+]
+
+export default destinationCategoreis
