207e39e6ad0ca83d28d0fe0eb5d6edb40ee00026
hoshikwyw <khaingwutyiwin1712@gmail.com>
2024-10-28 16:30:45 +0630

    Card details page done

--- components/GroupListing.tsx
+++ components/GroupListing.tsx
@@ -45,6 +45,7 @@ const styles = StyleSheet.create({
     item: {
         backgroundColor: Colors.white,
         padding: 10,
+        width: 330,
         borderRadius: 10,
         marginRight: 20,
         flexDirection: "row",
